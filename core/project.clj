7623f1119aa15239de9cc1bcb09ddc5f165494c7
Nico Rikken <nico@nicorikken.eu>
2017-08-24 13:55:46 +0200

    wip: include source paths and copyright information

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,11 @@
 (defproject com.alliander.chainsaw/core "0.1.0"
+  :author "Alliander NV <https://www.alliander.com/>"
+  :description "Data driven logging library for taoensso/timbre"
+  :url "https://github.com/aliander/chainsaw"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
+
   :scm {:dir ".."}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.taoensso/encore "2.91.1"]
--- elastic/project.clj
+++ elastic/project.clj
@@ -1,6 +1,12 @@
 (defproject com.alliander.chainsaw/elastic "0.1.0"
+  :author "Alliander NV <https://www.alliander.com/>"
+  :description "Data driven logging library for taoensso/timbre"
+  :url "https://github.com/aliander/chainsaw"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
+
   :scm {:dir ".."}
-  :source-paths ["src" "../core/src"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [clojurewerkz/elastisch "3.0.0-beta2"]]
@@ -8,4 +14,5 @@
   :test-selectors {:default     (complement (some-fn :integration :loadtest))
                    :integration :integration
                    :loadtest    :loadtest
-                   :all         (constantly true)})
+                   :all         (constantly true)}
+  :profiles {:dev {:source-paths ["src" "../core/src"]}})
